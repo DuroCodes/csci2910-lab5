82fd3a23b65a80ded7e095d8c36680336bf4b9b2
DuroCodes <davidwright13503@gmail.com>
2025-09-25 21:50:22 -0400

    feat: add necessary dataclasses

---
+++ main.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class Pokemon:
+    name: str
+    types: list[str]
+    abilities: list[str]
+    stats: dict[str, int]
+
+@dataclass
+class PokemonSet:
+    name: str
+    moves: list[str]
+    ability: str
+    item: str
+    nature: str
+    evs: dict[str, int]
+
+@dataclass
+class TeamMember:
+    pokemon: Pokemon
+    set: PokemonSet
+
